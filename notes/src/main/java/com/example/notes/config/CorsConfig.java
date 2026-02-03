692fcfd39496b6166127793772db8240c3cf45c1
Attoh Ifeanyi <alexander.attoh22@gmail.com>
2026-02-03 15:51:52 +0100

    feat: live frontend url fix

--- notes/src/main/java/com/example/notes/config/CorsConfig.java
+++ notes/src/main/java/com/example/notes/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:9010", "https://notes-ui-production.up.railway.app/")
+                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:9010", "https://notes-ui-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
