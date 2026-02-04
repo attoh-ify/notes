97c71390effd91bd4e7e545b0e8dc9e0c3665ff6
Attoh Ifeanyi <alexander.attoh22@gmail.com>
2026-02-04 12:41:17 +0100

    feat: undo last commit

--- notes/src/main/java/com/example/notes/config/SecurityConfig.java
+++ notes/src/main/java/com/example/notes/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
                                 "/index.html",
                                 "/css/**",
                                 "/js/**",
-                                "/api/relay/info/**",
+                                "/api/relay/**",
                                 "/api/users/login",
                                 "/api/users/register"
                         ).permitAll()
