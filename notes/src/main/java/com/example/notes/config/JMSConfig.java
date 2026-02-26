4dea406dbf66a363fff8e19046bcd57bfd555a08
Attoh Ifeanyi <114439637+attoh-ify@users.noreply.github.com>
2026-02-26 15:35:33 +0100

    feat: fix JmsConfig

--- notes/src/main/java/com/example/notes/config/JMSConfig.java
+++ notes/src/main/java/com/example/notes/config/JMSConfig.java
@@ -1,33 +1,14 @@
 package com.example.notes.config;
 
 import jakarta.jms.ConnectionFactory;
-import org.apache.qpid.jms.JmsConnectionFactory;
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.core.JmsTemplate;
 
 @Configuration
 @EnableJms
 public class JMSConfig {
-    @Value("${spring.activemq.broker-url:vm://localhost?broker.persistent=false}")
-    private String brokerUrl;
-
-    @Bean
-    public ConnectionFactory activeMQConnectionFactory() {
-        if (brokerUrl.startsWith("amqp")) {
-            return new JmsConnectionFactory(brokerUrl);
-        } else {
-            ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
-            factory.setBrokerURL(brokerUrl);
-            factory.setTrustAllPackages(true);
-            return factory;
-        }
-    }
-
     @Bean
     public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory) {
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
@@ -35,10 +16,5 @@ public class JMSConfig {
         factory.setPubSubDomain(false);
         factory.setConcurrency("1");
         return factory;
-    }
-
-    @Bean
-    public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
-        return new JmsTemplate(connectionFactory);
     }
 }
