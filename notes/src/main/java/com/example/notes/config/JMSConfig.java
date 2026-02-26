148b7dc158b0e04e38ba430ae978b8162a3164e2
Attoh Ifeanyi <114439637+attoh-ify@users.noreply.github.com>
2026-02-26 13:29:00 +0100

    feat: fix JmsConfig connection

--- notes/src/main/java/com/example/notes/config/JMSConfig.java
+++ notes/src/main/java/com/example/notes/config/JMSConfig.java
@@ -1,21 +1,24 @@
 package com.example.notes.config;
 
+import jakarta.jms.ConnectionFactory;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
 
 @Configuration
 @EnableJms
 public class JMSConfig {
-    private final static String ACTIVEMQ_URL = "tcp://localhost:61616";
 
     @Bean
-    public ActiveMQConnectionFactory activeMQConnectionFactory() {
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-        connectionFactory.setBrokerURL(ACTIVEMQ_URL);
-        return connectionFactory;
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setPubSubDomain(false);
+        factory.setConcurrency("5-20");
+        return factory;
     }
 
     @Bean
