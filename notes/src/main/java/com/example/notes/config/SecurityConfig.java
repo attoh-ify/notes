edbe57cfaae2494099003754c429e685a288a43b
Attoh Ifeanyi <114439637+attoh-ify@users.noreply.github.com>
2026-05-06 16:32:09 +0100

    fix: security config

--- notes/src/main/java/com/example/notes/config/SecurityConfig.java
+++ notes/src/main/java/com/example/notes/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Cache-Control"));
-        config.setAllowCredentials(true);
+        config.setAllowCredentials(false);
 
         config.setExposedHeaders(List.of("Authorization"));
 
